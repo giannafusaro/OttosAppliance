9c53705d7fc5dacd37dd98ab88709002cef1bd65
Gianna Fusaro <giannafusaro389@gmail.com>
2014-05-18 00:43:27 -0700

    Add user login & has_secure_password

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,7 @@
 class UserController < ApplicationController
 
   def login
-
+    @user = User.new
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,8 @@ Rails.application.routes.draw do
 
   root 'user#login'
 
+  resources :users
+
   resources :invoices
 
 end
