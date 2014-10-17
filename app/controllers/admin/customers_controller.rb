511625bd9f597ca90c5bd24ed01b34fe1af506b9
Gianna Fusaro <giannafusaro389@gmail.com>
2014-10-17 00:53:07 -0700

    Customer crud

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,54 @@
+class Admin::CustomersController < ApplicationController
+
+  def index
+    @customers = Customer.all
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new customer_params
+
+    if @customer.save
+      flash[:success] = "Customer created successfully!"
+      redirect_to admin_customers_path
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find params[:id]
+    if @customer.update_attributes(customer_params)
+      flash[:success] = "Customer updated successfully!"
+      redirect_to admin_customers_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id]).destroy
+    flash[:success] = "Customer destroyed successfully."
+    redirect_to admin_customers_path
+  end
+
+private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :street_address, :city,
+      :zip_code, :phone, :notes)
+  end
+
+end
